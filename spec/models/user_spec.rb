6381e4c0eb0fdbab52d4e9625dc05cfc17d8f904
marvinkhristianzamora <mklzamora@gmail.com>
2012-09-26 13:56:46 +0800

    Add user email validation w/ tests
    Signed-off-by: marvinkhristianzamora <mklzamora@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,4 +35,24 @@ describe User do
     before { @user.email = " " }
     it { should_not be_valid }
   end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_food.org example.user@foo. foo@bar_raz.org foot@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when eamil format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org first.lst@foo.jp a+b@baz.com]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
 end
