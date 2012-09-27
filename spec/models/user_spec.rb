18636dabc9724325ee75713a6102dcb0b5432c0b
marvinkhristianzamora <mklzamora@gmail.com>
2012-09-27 11:07:27 +0800

    Add app based user email validation
    Signed-off-by: marvinkhristianzamora <mklzamora@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,7 +41,7 @@ describe User do
       addresses = %w[user@foo,com user_at_food.org example.user@foo. foo@bar_raz.org foot@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
-        @user.should_not be_valid
+        should_not be_valid
       end
     end
   end
@@ -51,8 +51,18 @@ describe User do
       addresses = %w[user@foo.COM A_US-ER@f.b.org first.lst@foo.jp a+b@baz.com]
       addresses.each do |valid_address|
         @user.email = valid_address
-        @user.should be_valid
+        should be_valid
       end
     end
+  end
+
+  describe "when email address is already taken" do
+    before do 
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
   end
 end
